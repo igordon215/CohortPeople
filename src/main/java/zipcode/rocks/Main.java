ccfc438c04a3aa65d799d976dfe4135f24a47412
igordon215 <igordon215@gmail.com>
2024-06-27 21:39:31 -0400

    I have added changes

--- src/main/java/zipcode/rocks/Main.java
+++ src/main/java/zipcode/rocks/Main.java
@@ -12,12 +12,12 @@ public class Main {
     private static final String COMMA_DELIMITER = ",";
 
     public static void main(String[] args) {
-        String student_source = "FakeData.csv";
+        String student_source = "TenFiveZero.csv";
 
         // Load data file into an ArrayList<Person> of Person objects.
         Main programObject = new Main();
 
-        List listOfStudents = programObject.loadCSVFile(student_source);
+        List listOfStudents = programObject.handWrittenLoadOfData();;
 
         int numberOfPersons = listOfStudents.size();
         for (int i = 0; i< numberOfPersons; i++){
@@ -43,9 +43,125 @@ public class Main {
 
     private Person getRecordFromLine(String line) {
         // take the String of lastname,firstname and create a new Person object with it.
+
         String[] values = line.split(COMMA_DELIMITER);
-        Person newPerson = new Person(values[0], values[1]);
-        return newPerson;
+       /* Person newPerson = new Person (values[0], values[1],values[2], values[3], values[4], values[5]);*/
+        return null; /*return new Person()*/ //FOR CSV FILE
     }
+
+    private List handWrittenLoadOfData() {
+        ArrayList<Person> students = new ArrayList<>();
+
+        Person newPerson = new Person("Teddy", "Archibald");
+        newPerson.setFavdrink("Half&Half");
+        newPerson.setFavcolor("Red");
+        newPerson.setBirthmonth("March");
+        newPerson.setAnypets(false);
+        students.add(newPerson);
+
+        Person newPerson1 = new Person("Peter", "VanBeever");
+        newPerson.setFavdrink("Latte");
+        newPerson.setFavcolor("Purple");
+        newPerson.setBirthmonth("February");
+        newPerson.setAnypets(true);
+        students.add(newPerson1);
+
+        Person newPerson2 = new Person("Tim", "Linkous");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Green");
+        newPerson.setBirthmonth("September");
+        newPerson.setAnypets(false);
+        students.add(newPerson2);
+
+        Person newPerson3 = new Person("Qian", "Wang");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Blue");
+        newPerson.setBirthmonth("October");
+        newPerson.setAnypets(false);
+        students.add(newPerson3);
+
+        Person newPerson4 = new Person("Diptika", "Devi");
+        newPerson.setFavdrink("Tea");
+        newPerson.setFavcolor("Orange");
+        newPerson.setBirthmonth("June");
+        newPerson.setAnypets(false);
+        students.add(newPerson4);
+
+        Person newPerson5 = new Person("Ian", "Gordon");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Green");
+        newPerson.setBirthmonth("April");
+        newPerson.setAnypets(true);
+        students.add(newPerson5);
+
+        Person newPerson6 = new Person("Tim", "Nguyen");
+        newPerson.setFavdrink("Bubble Tea");
+        newPerson.setFavcolor("White");
+        newPerson.setBirthmonth("November");
+        newPerson.setAnypets(true);
+        students.add(newPerson6);
+
+        Person newPerson7 = new Person("Bryan", "Smith");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Brown");
+        newPerson.setBirthmonth("May");
+        newPerson.setAnypets(false);
+        students.add(newPerson7);
+
+        Person newPerson8 = new Person("Jonathan", "Diehl");
+        newPerson.setFavdrink("Coffee");
+        newPerson.setFavcolor("Green");
+        newPerson.setBirthmonth("February");
+        newPerson.setAnypets(true);
+        students.add(newPerson8);
+
+        Person newPerson9 = new Person("Jared", "Thacker");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Green");
+        newPerson.setBirthmonth("December");
+        newPerson.setAnypets(false);
+        students.add(newPerson9);
+
+        Person newPerson10 = new Person("Robbie", "Niemeyer");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Blue");
+        newPerson.setBirthmonth("January");
+        newPerson.setAnypets(false);
+        students.add(newPerson10);
+
+        Person newPerson11 = new Person("Mohammed", "N.");
+        newPerson.setFavdrink("Snapple");
+        newPerson.setFavcolor("Green");
+        newPerson.setBirthmonth("August");
+        newPerson.setAnypets(false);
+        students.add(newPerson11);
+
+        Person newPerson12 = new Person("Josue", "Castro");
+        newPerson.setFavdrink("Coffee");
+        newPerson.setFavcolor("Blue");
+        newPerson.setBirthmonth("June");
+        newPerson.setAnypets(true);
+        students.add(newPerson12);
+
+        Person newPerson13 = new Person("Coreye", "Ross");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Black");
+        newPerson.setBirthmonth("October");
+        newPerson.setAnypets(false);
+        students.add(newPerson13);
+
+        Person newPerson14 = new Person("Will", "Chapman");
+        newPerson.setFavdrink("Water");
+        newPerson.setFavcolor("Purple");
+        newPerson.setBirthmonth("December");
+        newPerson.setAnypets(false);
+        students.add(newPerson14);
+
+        // and so on for EACH person in the cohort.
+
+        return students;
+    }
+
+
 
 }
--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -3,11 +3,29 @@ package zipcode.rocks;
 public class Person {
     private String firstname;
     private String lastname;
+    private String favdrink;
+    private String favcolor;
+    private String birthmonth;
+    private boolean anypets;
 
-    public Person(String fname, String lname) {
+
+// THIS IS FOR THE CSV FILE
+    /*public Person(String fname, String lname, String fdrink, String fcolor, String bmonth, String pets) {
+        this.firstname = fname;
+        this.lastname = lname;
+        this.favdrink = fdrink;
+        this.favcolor = fcolor;
+        this.birthmonth = bmonth;
+        this.anypets = Boolean.valueOf(pets);
+    } */
+
+    public Person(String fname, String lname){
         this.firstname = fname;
         this.lastname = lname;
     }
+
+
+
 
 
     public String getFirstname() {
@@ -26,7 +44,51 @@ public class Person {
         this.lastname = lastname;
     }
 
-    public String toString() {
-        return this.lastname + ", " + this.firstname;
+    public String getFavdrink() {
+        return favdrink;
     }
+
+    public void setFavdrink(String favdrink) {
+        this.favdrink = favdrink;
+    }
+
+    public String getFavcolor() {
+        return favcolor;
+    }
+
+    public void setFavcolor(String favcolor) {
+        this.favcolor = favcolor;
+    }
+
+    public String getBirthmonth() {
+        return birthmonth;
+    }
+
+    public void setBirthmonth(String birthmonth) {
+        this.birthmonth = birthmonth;
+    }
+
+    public boolean getAnypets() {
+        return anypets;
+    }
+
+    public void setAnypets(boolean anypets) {
+        this.anypets = anypets;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", favdrink='" + favdrink + '\'' +
+                ", favcolor='" + favcolor + '\'' +
+                ", birthmonth='" + birthmonth + '\'' +
+                ", anypets='" + anypets + '\'' +
+                '}';
+        }
+
+    //public String toString() {
+    //    return this.lastname + ", " + this.firstname;
+    // }
 }
